4cda2e7a0a0a640e06be370c3bd1e323d9838b5b
pete101011 <lee1509@purdue.edu>
2015-11-06 21:21:42 -0500

    API and waiting on frontend update

--- js/controller.js
+++ js/controller.js
@@ -1,19 +1,60 @@
+//Global variable Listing
+var turn = 'w';
+var position;
+var boardID = 0;
+var color = 'w';
 
 var onDrop = function(source, target, piece, newPos, oldPos, orientation) {
-  console.log("Source: " + source);
-  console.log("Target: " + target);
-  console.log("Piece: " + piece);
-  console.log("New position: " + ChessBoard.objToFen(newPos));
-  console.log("Old position: " + ChessBoard.objToFen(oldPos));
-  console.log("Orientation: " + orientation);
-  console.log("--------------------");
+	console.log("Source: " + source);
+	console.log("Target: " + target);
+	console.log("Piece: " + piece);
+	console.log("Orientation: " + orientation);
+	console.log("--------------------");
+	position = board.position();
+	tryMove(source, target, piece[0]);
+	return 'snapback';
 };
 
 var cfg = {
-  draggable: true,
-  position: 'start',
-  onDrop: onDrop,
-  sparePieces: false
+	draggable: true,
+	position: 'start',
+	onDrop: onDrop,
+	sparePieces: false
 };
 
-var board1 = ChessBoard('board1', cfg);
+var tryMove = function(source, target, color) {
+	var moveRequest = $.post( "/games/"+boardID+"/move", {source: source, target: target, color: color}, function(data) {
+		board.move(source + '-' + target);
+	})
+	.done(function() {
+		
+	})
+	.fail(function(e) {
+		console.log("Server returned a "+ e.status +" "+ e.statusText)
+		//ADD MORE FOR DIFFERENT ERRORS
+	})
+	.always(function() {
+		
+	});
+	
+	
+}
+
+
+var board = ChessBoard('board1', cfg);
+
+var getBoard = function() {
+	var statusRequest = $.get( "/games/"+boardID+"/status", function(data) {
+		
+	})
+	.done(function() {
+		
+	})
+	.fail(function() {
+		
+	})
+	.always(function() {
+		
+	});
+
+}
